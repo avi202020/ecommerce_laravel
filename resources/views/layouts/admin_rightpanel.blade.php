b0bb498dd5707f3019903c1c349d6712fbc7125d
lucassantosf <lucas46em@gmail.com>
2021-01-11 17:46:01 -0300

    Feature: created layout with includes and sections blade laravel

---
+++ resources/views/layouts/admin_rightpanel.blade.php
@@ -0,0 +1,12 @@
+<!-- Right Panel -->
+<div id="right-panel" class="right-panel">
+
+    @include('layouts.admin_header')
+
+    @yield('content')
+
+    <div class="clearfix"></div>
+
+    @include('layouts.admin_footer')
+</div>
+<!-- /#right-panel -->
